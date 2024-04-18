e2969b3a27d32a293d05d9c4d6564ee24ff1da6e
Bapti <torresnanninivalentino@gmail.com>
2024-04-18 15:51:31 -0300

    comit

---
+++ while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//for (int iterador = 1; iterador <= 10; iterador++){
+//    int valor = 2;
+//    valor = valor * iterador;
+//    cout << valor << endl;
+//}
+
+int valor = 42;
+for (int iterador = 0; iterador < valor; iterador++){
+    if (iterador%2 == 0){
+     cout << iterador << endl;
+    }
+}
+}
